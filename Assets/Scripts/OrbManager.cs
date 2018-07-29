57c1244d96137fb51b8273bbd3616f9d4da343e5
佐伯紘二 <saikou9901@gmail.com>
2018-07-29 15:14:57 +0900

    Implement the logic catching orb.

--- Assets/Scripts/OrbManager.cs
+++ Assets/Scripts/OrbManager.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class OrbManager : MonoBehaviour {
 
+	private GameObject gameManager;
+
 	// Use this for initialization
 	void Start () {
-		
+		gameManager = GameObject.Find("GameManager");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void TouchOrb () {
+		if (Input.GetMouseButton (0) == false) {
+			return;
+		}
+
+		gameManager.GetComponent<GameManager> ().GetOrb ();
+		Destroy (this.gameObject);
 	}
 }
